522762c1829ec76eb4564cb215cbd114d13633ac
Christoph Taucher <taucher.ch@gmail.com>
2018-09-07 19:11:30 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Value, TooltipConfiguration } from 'dl-chart';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  values: Value[] = [
+    new Value('Red', 5, 'Red', null, new TooltipConfiguration(null, (val, perc) => { return val.name + ' ( ' + perc + '% )' })),
+    new Value('Blue', 3, 'Blue'),
+    new Value('Green', 10, 'Green', null, new TooltipConfiguration(null, (val, perc) => { return val.name + ' (' + val.value + ')' })),
+    new Value('Orange', 3, 'Orange'),
+  ];
+
+  filterValues: Value[] = this.values;
+  filterValuesBar: Value[] = this.values;
+
+  onKey(event: any) {
+    let inputValue = event.target.value;
+    this.filterValues = this.values.filter(val => {
+      if (inputValue === undefined || inputValue === '') {
+        return true;
+      }
+      if (val.name === inputValue || val.value.toString() === inputValue) {
+        return true;
+      }
+      return false;
+    });
+  }
+
+  onKeyBar(event: any) {
+    let inputValue = event.target.value;
+    this.filterValuesBar = this.values.filter(val => {
+      if (inputValue === undefined || inputValue === '') {
+        return true;
+      }
+      if (val.name === inputValue || val.value.toString() === inputValue) {
+        return true;
+      }
+      return false;
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { DlChartModule } from "dl-chart";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DlChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
